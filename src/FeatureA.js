beb30c4d15ba664089bfac38267344b701263289
Krishna2427 <krishnalankapally8@gmail.com>
2025-06-14 12:53:20 +0530

    Added project files

--- src/FeatureA.js
+++ src/FeatureA.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function FeatureA() {
-  return <p>This is Feature A component</p>;
+  return <p>This is Feature A component Revathi</p>;
 }
 
 export default FeatureA;
