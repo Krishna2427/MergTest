9415454514d383ac5b86edfb769b22d2f42295d0
Krishna2427 <krishnalankapally8@gmail.com>
2025-06-14 11:48:59 +0530

    Added project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FeatureB from "./FeatureB";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Merge Test App</h1>
+      <FeatureB />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FeatureB.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FeatureB() {
+  return <p>This is Feature B component</p>;
+}
+
+export default FeatureB;
